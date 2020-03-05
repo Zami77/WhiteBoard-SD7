73a91fbe21104df74b94335b977ae60c64af00cd
Daniel Lungaro <chunk_77@hotmail.com>
2020-03-05 09:38:55 -0800

    Added MediaPlayer interface

---
+++ WhiteboardSD7/MediaPlayerDemo/MediaPlayers.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaPlayers
+{
+    public interface IStreamingDevice
+    {
+        public string DeviceName { get; set; }
+        public bool IsPlaying { get; set; }
+        bool Play();
+        void Pause();
+        void Ffwd();
+        void Rewind();
+        bool Stop();
+    }
+    
+    public class DVDPlayer : IStreamingDevice
+    {
+        
+        public string DeviceName { get; set; }
+        public bool IsPlaying { get; set; }
+
+        public void Ffwd()
+        {
+            Console.WriteLine($"FFwd {DeviceName}");
+        }
+        public DVDPlayer()
+        {
+            DeviceName = "Generic DVD Player";
+            IsPlaying = false;
+        }
+        public void Pause()
+        {
+            if (IsPlaying)
+            {
+                Console.WriteLine($"{DeviceName} paused");
+                IsPlaying = false;
+                return;
+            }
+            Console.WriteLine($"{DeviceName} is not playing");
+        }
+
+        public bool Play()
+        {
+            if (IsPlaying) 
+            { 
+                Console.WriteLine($"{DeviceName} is already playing");
+                return IsPlaying;
+            }
+            Console.WriteLine($"{DeviceName} is now playing");
+            IsPlaying = true;
+            return IsPlaying;
+        }
+
+        public void Rewind()
+        {
+            Console.WriteLine($"Rewind {DeviceName}");
+        }
+        public bool Stop()
+        {
+            if(!IsPlaying)
+            {
+                Console.WriteLine($"{DeviceName} is already stopped");
+                return IsPlaying;
+            }
+            Console.WriteLine($"Stop {DeviceName}");
+            IsPlaying = false;
+            return IsPlaying;
+
+        }
+    }
+}
---
+++ WhiteboardSD7/MediaPlayerDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MediaPlayers;
+
+namespace MediaPlayerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Media Player Interface Demo");
+
+            DVDPlayer mediaPlayer1 = new DVDPlayer();
+            Console.WriteLine($"{mediaPlayer1.DeviceName}");
+            mediaPlayer1.DeviceName = "DVD Player";
+            Console.WriteLine($"{mediaPlayer1.DeviceName}");
+            string input;
+            Console.WriteLine(">.Press Play\nP.Press Pause\nF.Press FFwd\nR.Press Rewind\nS.Press Stop\n0.To Exit");
+            while (true)
+            {
+                input = Console.ReadLine();
+                switch(input)
+                {
+                    case ">":
+                        mediaPlayer1.Play();
+                        break;
+                    case "P":
+                        mediaPlayer1.Pause();
+                        break;
+                    case "F":
+                        mediaPlayer1.Ffwd();
+                        break;
+                    case "R":
+                        mediaPlayer1.Rewind();
+                        break;
+                    case "S":
+                        mediaPlayer1.Stop();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Incorrect Input, try again...");
+                        break;
+                }
+            }
+
+
+
+        }
+    }
+}
